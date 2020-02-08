9feca24449f40508acce6c8cedbd5194a14a256a
Charlie <cspindler@mail.com>
2020-02-07 23:43:18 -0700

    theramin_build5 tests data ready in user  loop, runs 16x faster

---
+++ Arduino/theramin_build5/theramin_build5.ino
@@ -0,0 +1,347 @@
+// theramin_build5.ino by C.Spindler 2-7-2020
+// based on theramin_build4.ino which is working well,
+// but is using 32 ms per loop{...}.
+// I just tested ...build5 at 11:30 pm on 2-7-2020 and 
+// it is using 1.8 ms per loop{...}
+
+
+// the point of ...build5 is to use public functions of the Pololu library
+// for controlling the VL53L0x devices, which allow us to determine if a measurement
+// is ready to be read, and then only read a TOF sensor when we know we will not wait.
+
+
+#define ENABLE_SERIAL // comment out to drop compilation of serial print code
+#define ENABLE_STOPWATCH // comment out unless you want to to measure elapsed time between loops
+
+// from Single_Buzzer.ino 
+#include <Wire.h> // not in ...build3 because it's clearly in Adafruit_VL53L0X.h
+#include <VL53L0X.h> // this is the header for the Pololu library for vl53l0x
+#define SPEAKER  6      // use pin D6 on Nano
+
+
+
+// from spi_demo_nano.ino and is used to enable the digital potentiomenter
+#include <SPI.h>  // comment this out if your radio library establishs SPI
+#define SS1  7 //Pin 7 is SS (slave select) for device 1
+#define REG0 B00000000 //Register 0 Write command for Px0 pins
+#define REG1 B00010000 //Register 1 Write command for Px1 pins
+
+
+// address we will assign if dual sensor is present
+#define SENSOR1_ADDRESS 0x33
+#define SENSOR2_ADDRESS 0x35
+#define SENSOR3_ADDRESS 0x37
+
+// set the pins to shutdown
+#define SHT_SENSOR1 2  // D2 is the Nano pin conected to XSHUT for sensor1
+#define SHT_SENSOR2 3  // D3 is the Nano pin conected to XSHUT for sensor2
+#define SHT_SENSOR3 4  // D4 is the Nano pin conected to XSHUT for sensor1
+
+
+ VL53L0X sensor1; // constructor for Pololu's sensor control objects
+ VL53L0X sensor2; 
+ VL53L0X sensor3; 
+
+
+/* 
+    in  void setIdAndInit() 
+	(  in the new arrangement using the Pololu code library, VL53L0X.cpp:
+			initialization is by using:   		bool VL53L0X::init(bool io_2v8)
+			i2c address assignment is by: 		void VL53L0X::setAddress(uint8_t new_address)  )
+	  1. Reset all sensors by setting all of their XSHUT pins low ,
+    2. then, one sensor at at time:
+        a. after 10 ms, activate the sensor by making its XSHUT pin high, and leaving it high for rest of sketch
+        b. after 10 ms, initiate sensor using bool VL53L0X::init(bool io_2v8)
+        c. after 10 ms, assign sensor its new address using void VL53L0X::setAddress(uint8_t new_address) 
+    3. Pick any number but 0x29 and whatever addres the other sensors areassigned.  Addresses must be under 0x7F. 
+       Going with 0x30 to 0x3F is probably OK.
+    4. 10 ms after last address is assigned, start continuous reading of sensors, one sensor object at a time,
+       using void VL53L0X::startContinuous();
+	  
+ */
+void setIdAndInit() {
+ 
+    // all reset
+    
+  digitalWrite(SHT_SENSOR1, LOW);    
+  digitalWrite(SHT_SENSOR2, LOW);
+  digitalWrite(SHT_SENSOR3, LOW);
+  delay(10);
+  
+  // activating SENSOR1 
+  digitalWrite(SHT_SENSOR1, HIGH);
+  delay(10);
+
+  //init SENSOR1
+  if (!sensor1.init())
+  {
+    Serial.println("Failed to detect and initialize first sensor!");
+    while (1) {}
+  }
+  
+  // setting address for SENSOR1
+  sensor1.setAddress(SENSOR1_ADDRESS);
+  delay(10);
+
+  //  activate SENSOR2
+  digitalWrite(SHT_SENSOR2, HIGH);
+  delay(10);
+  
+  // init SENSOR2
+  if (!sensor2.init())
+  {
+    Serial.println("Failed to detect and initialize second sensor!");
+    while (1) {}
+  }
+  // setting address for SENSOR2
+  sensor2.setAddress(SENSOR2_ADDRESS);
+  delay(10);
+
+  //  activate SENSOR3
+  digitalWrite(SHT_SENSOR3, HIGH);
+  delay(10);
+  
+  // init SENSOR3
+  if (!sensor3.init())
+  {
+    Serial.println("Failed to detect and initialize third sensor!");
+    while (1) {}
+  }
+  // setting address for SENSOR3
+  sensor3.setAddress(SENSOR3_ADDRESS);
+  delay(10);
+
+  sensor1.startContinuous(); // necessary to use CONTINUOUS mode
+  sensor2.startContinuous();
+  sensor3.startContinuous();
+ 
+}
+
+void read_dual_sensors() {  
+
+// the following line is the same test that is used by
+// uint16_t VL53L0X::readRangeContinuousMillimeters(void)
+  if((sensor1.readReg(VL53L0X::RESULT_INTERRUPT_STATUS) & 0x07) != 0) {
+     uint16_t dist1 = sensor1.readRangeContinuousMillimeters();
+      if (dist1 != 65535) {      // bool distNValid = (distN != 65535);
+       // print sensor one reading
+#ifdef ENABLE_SERIAL 
+       Serial.print("  1: ");
+       Serial.print(dist1);
+ #endif
+       pitch(dist1);
+    }
+  }
+
+// the following line is the same test that is used by
+// uint16_t VL53L0X::readRangeContinuousMillimeters(void)
+    if((sensor2.readReg(VL53L0X::RESULT_INTERRUPT_STATUS) & 0x07) != 0) {
+      uint16_t dist2 = sensor2.readRangeContinuousMillimeters();
+      if (dist2 != 65535) {   // bool distNValid = (distN != 65535);
+      // print sensor one reading
+#ifdef ENABLE_SERIAL 
+     Serial.print("  2: ");
+     Serial.print(dist2);
+ #endif
+      volume(dist2);
+   }
+ }
+
+// the following line is the same test that is used by
+// uint16_t VL53L0X::readRangeContinuousMillimeters(void)
+ if((sensor3.readReg(VL53L0X::RESULT_INTERRUPT_STATUS) & 0x07) != 0) {
+    uint16_t dist3 = sensor3.readRangeContinuousMillimeters();
+  if (dist3 != 65535) {   // bool distNValid = (distN != 65535);
+    // print sensor one reading
+#ifdef ENABLE_SERIAL 
+    Serial.print("  3: ");
+    Serial.print(dist3);
+    Serial.println(); // take out if using controlFunction(dist3);
+ #endif
+    // controlFunction(dist3);  goes here, null for now
+     }
+   }
+}
+
+void pitch(uint16_t dist){
+
+   #define MAXDISTANCE  1200
+   static uint16_t last_frequency;
+   uint16_t frequency = dist; //need not be identically equal
+
+   if(dist <= MAXDISTANCE ){
+          tone(SPEAKER, frequency );
+          last_frequency = frequency; 
+#ifdef ENABLE_SERIAL
+          Serial.print("  frequency = ");
+          Serial.print(frequency);
+          
+          } else {
+            //  noTone(SPEAKER); // comment out noTone() to sustain last tone
+            Serial.print("  last_frequency = "); 
+            Serial.print( last_frequency);
+          }
+#endif
+}
+
+void commandPot(int SS, int reg, int level) {
+  // from spi_demo_nano.ino
+  
+ // SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
+ // only use SPI.beginTransaction(...) & SPI.endTransaction(...) in 
+ // coordination with your radio library, which uses SPI internally
+   
+  digitalWrite(SS, LOW); // set the named pin LOW 
+                         // to select that chip
+  SPI.transfer(reg  ); // send the first byte to pick the register 
+                       // number of the potentionmeter of the selected chip
+  SPI.transfer(level); // send the 2nd byte to set the level of that 
+                       // potentiometer
+  digitalWrite(SS,HIGH); // de-select that chip             
+
+ //  SPI.endTransaction();
+                                             
+}
+
+void volume(uint16_t dist){
+
+// map distances from TOF sensor  to the 129 steps,
+// (0 to 128) of the MCP4231 103E digital potentiometer
+
+#define MAXDISTANCE2 800
+
+#define BOTTOMRANGE 30
+#define TOPRANGE 600
+#define LOSTEP 0
+#define HISTEP 128
+
+uint16_t volumeLevel;
+static uint16_t lastVolumeLevel; 
+
+if (dist >= MAXDISTANCE2){
+     volumeLevel = lastVolumeLevel;
+#ifdef ENABLE_SERIAL  
+    Serial.print(" lastVolumeLevel = ");
+    Serial.print(volumeLevel);
+#endif
+    }else {
+
+uint16_t rangeStep = (TOPRANGE-BOTTOMRANGE)/(HISTEP - LOSTEP);
+volumeLevel = LOSTEP;
+if (dist >= TOPRANGE ){
+    volumeLevel = HISTEP;
+    } else { 
+        if (dist > (BOTTOMRANGE + rangeStep )){
+          // Careful, intermediate product may be too big for unit16_t and 
+          // would be automatically promoted by the compiler to uint32_t. 
+          // Also, assure that for all values of dist that can reach this
+          // statement, the result in volumeLevel are in the range from
+          // ( LOSTEP+1 ) to ( HISTEP-1 ), and that no intermediate arithemetic 
+          // results are less than zero or higher than uint32_t can hold.
+          volumeLevel = (uint16_t)((dist-BOTTOMRANGE)*HISTEP/(TOPRANGE-BOTTOMRANGE));
+        }
+    }        
+
+    lastVolumeLevel = volumeLevel;
+    #ifdef ENABLE_SERIAL  
+    Serial.print(" volumeLevel = ");
+    Serial.print(volumeLevel);
+    #endif
+    }
+    
+    commandPot(SS1 ,REG0, volumeLevel); // sets wiper on pot pins P0W
+    commandPot(SS1 ,REG1, volumeLevel); // sets wiper on pot pins P1W
+  
+}
+
+
+void setup() {
+
+  // from spi_demo_nano.ino
+
+  // setup slave select pins for output
+  pinMode(SS1, OUTPUT);
+
+  // per instructions from arduino.cc/en/reference/SPI 
+  // set the Nano's SS pin, 11, to OUTPUT to make it 
+  // unuasable as a chip select for whole Nano board as a SPI slave
+  // also, we use pin D11 for MOSI, which is compatable with OUTPUT
+  pinMode(11,OUTPUT);
+
+//  initialize SPI 
+    SPI.begin(); // comment this out if your radio library initializes SPI
+
+
+pinMode(SHT_SENSOR1,OUTPUT);
+pinMode(SHT_SENSOR2,OUTPUT);
+pinMode(SHT_SENSOR3,OUTPUT);
+
+
+  #ifdef ENABLE_SERIAL 
+  Serial.begin(115200);
+  // wait until serial port opens for native USB devices
+  while (! Serial) { delay(1); }
+  #endif
+
+  #ifdef ENABLE_STOPWATCH
+     #ifndef ENABLE_SERIAL 
+     Serial.begin(115200);
+     // wait until serial port opens for native USB devices
+     while (! Serial) { delay(1); }
+     Serial.println("STOPWATCH enabled");
+     #endif
+  #endif
+
+  Wire.begin();
+
+  setIdAndInit();
+
+
+  #if defined LONG_RANGE
+  // lower the return signal rate limit (default is 0.25 MCPS)
+  sensor1.setSignalRateLimit(0.1);
+  sensor2.setSignalRateLimit(0.1);
+  // increase laser pulse periods (defaults are 14 and 10 PCLKs)
+  sensor1.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
+  sensor1.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
+  sensor2.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
+  sensor2.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
+#endif
+
+#if defined HIGH_SPEED
+  // reduce timing budget to 20 ms (default is about 33 ms)
+sensor1.setMeasurementTimingBudget(20000);
+sensor2.setMeasurementTimingBudget(20000);
+#elif defined HIGH_ACCURACY
+  // increase timing budget to 200 ms
+  sensor1.setMeasurementTimingBudget(200000);
+  sensor2.setMeasurementTimingBudget(200000);
+#endif
+
+ 
+}
+
+void loop() {
+
+#ifdef ENABLE_STOPWATCH
+  static unsigned long time1 = millis();
+  static unsigned int k ; // by default, on creation, static is initalized to zero
+#endif
+  
+  read_dual_sensors();
+
+#ifdef ENABLE_STOPWATCH
+  k++;
+  if (k >= 100){
+     unsigned long time2 = millis();
+     Serial.print(" elapsed time for ");
+     Serial.print( k );
+     Serial.print(" loops is ");
+     Serial.print( (time2-time1));
+     Serial.println(" ms");
+     time1 = time2;
+     k=0;
+  }
+  
+#endif
+}
---
+++ doc/kicad/theramin/theramin2/theramin2.sch
@@ -0,0 +1,207 @@
+EESchema Schematic File Version 4
+EELAYER 30 0
+EELAYER END
+$Descr A4 11693 8268
+encoding utf-8
+Sheet 1 1
+Title ""
+Date ""
+Rev ""
+Comp ""
+Comment1 ""
+Comment2 ""
+Comment3 ""
+Comment4 ""
+$EndDescr
+$Comp
+L Device:Laserdiode_1A3C LD?
+U 1 1 5E3E4766
+P 2300 3080
+F 0 "LD?" H 2250 3365 50  0000 C CNN
+F 1 "Laserdiode_1A3C" H 2250 3274 50  0000 C CNN
+F 2 "" H 2200 3055 50  0001 C CNN
+F 3 "~" H 2330 2880 50  0001 C CNN
+	1    2300 3080
+	1    0    0    -1  
+$EndComp
+$Comp
+L Connector:RJ45 J?
+U 1 1 5E3E5EEE
+P 5060 2200
+F 0 "J?" H 4730 2204 50  0000 R CNN
+F 1 "RJ45" H 4730 2295 50  0000 R CNN
+F 2 "" V 5060 2225 50  0001 C CNN
+F 3 "~" V 5060 2225 50  0001 C CNN
+	1    5060 2200
+	-1   0    0    1   
+$EndComp
+$Comp
+L Device:R R?
+U 1 1 5E3E9A8A
+P 2880 3080
+F 0 "R?" V 2673 3080 50  0000 C CNN
+F 1 "100" V 2764 3080 50  0000 C CNN
+F 2 "" V 2810 3080 50  0001 C CNN
+F 3 "~" H 2880 3080 50  0001 C CNN
+	1    2880 3080
+	0    1    1    0   
+$EndComp
+Wire Wire Line
+	4660 2000 3230 2000
+Wire Wire Line
+	3230 2000 3230 2050
+Wire Wire Line
+	3230 2050 2590 2050
+Text Label 3960 2000 0    50   ~ 0
+orange_5v_unreg
+Text Label 3880 1900 0    50   ~ 0
+orange&white_GND
+$Comp
+L LIBRARY1:XL53L0X_POLOLU A?
+U 1 1 5E3E38FE
+P 2320 1850
+F 0 "A?" H 2253 1975 50  0000 C CNN
+F 1 "XL53L0X_POLOLU" H 2253 1884 50  0000 C CNN
+F 2 "" H 2320 1850 50  0001 C CNN
+F 3 "" H 2320 1850 50  0001 C CNN
+	1    2320 1850
+	1    0    0    -1  
+$EndComp
+Text Label 3930 2100 0    50   ~ 0
+green&white_GND
+Text Label 4180 2400 0    50   ~ 0
+green_SDA
+Wire Wire Line
+	4660 2400 3170 2400
+Wire Wire Line
+	3170 2400 3170 2250
+Wire Wire Line
+	3170 2250 2520 2250
+Wire Wire Line
+	4660 2200 3320 2200
+Wire Wire Line
+	3320 2200 3320 2350
+Wire Wire Line
+	3320 2350 2520 2350
+Text Label 4260 2200 0    50   ~ 0
+blue_SCL
+Wire Wire Line
+	4660 2600 3080 2600
+Wire Wire Line
+	3080 2600 3080 2450
+Wire Wire Line
+	3080 2450 2520 2450
+Text Label 4090 2600 0    50   ~ 0
+brown_XSHUT
+Text Label 3990 2300 0    50   ~ 0
+blue&white_GND
+Text Label 3930 2500 0    50   ~ 0
+brown&white_GND
+Wire Wire Line
+	2730 3080 2500 3080
+$Comp
+L power:GND #PWR?
+U 1 1 5E3F2EF4
+P 1770 3080
+F 0 "#PWR?" H 1770 2830 50  0001 C CNN
+F 1 "GND" H 1775 2907 50  0000 C CNN
+F 2 "" H 1770 3080 50  0001 C CNN
+F 3 "" H 1770 3080 50  0001 C CNN
+	1    1770 3080
+	1    0    0    -1  
+$EndComp
+Wire Wire Line
+	3030 3080 3030 2700
+Wire Wire Line
+	3030 2700 2590 2700
+Wire Wire Line
+	2590 2700 2590 2050
+Connection ~ 2590 2050
+Wire Wire Line
+	2590 2050 2520 2050
+Wire Wire Line
+	2520 2150 2790 2150
+$Comp
+L power:GND #PWR?
+U 1 1 5E3F4592
+P 2790 2150
+F 0 "#PWR?" H 2790 1900 50  0001 C CNN
+F 1 "GND" V 2795 2022 50  0000 R CNN
+F 2 "" H 2790 2150 50  0001 C CNN
+F 3 "" H 2790 2150 50  0001 C CNN
+	1    2790 2150
+	0    -1   -1   0   
+$EndComp
+$Comp
+L power:GND #PWR?
+U 1 1 5E3F56F3
+P 3830 1900
+F 0 "#PWR?" H 3830 1650 50  0001 C CNN
+F 1 "GND" V 3835 1772 50  0000 R CNN
+F 2 "" H 3830 1900 50  0001 C CNN
+F 3 "" H 3830 1900 50  0001 C CNN
+	1    3830 1900
+	0    1    1    0   
+$EndComp
+$Comp
+L power:GND #PWR?
+U 1 1 5E3F7418
+P 3830 2100
+F 0 "#PWR?" H 3830 1850 50  0001 C CNN
+F 1 "GND" V 3835 1972 50  0000 R CNN
+F 2 "" H 3830 2100 50  0001 C CNN
+F 3 "" H 3830 2100 50  0001 C CNN
+	1    3830 2100
+	0    1    1    0   
+$EndComp
+$Comp
+L power:GND #PWR?
+U 1 1 5E3F79BA
+P 3840 2300
+F 0 "#PWR?" H 3840 2050 50  0001 C CNN
+F 1 "GND" V 3845 2172 50  0000 R CNN
+F 2 "" H 3840 2300 50  0001 C CNN
+F 3 "" H 3840 2300 50  0001 C CNN
+	1    3840 2300
+	0    1    1    0   
+$EndComp
+$Comp
+L power:GND #PWR?
+U 1 1 5E3F80C8
+P 3850 2500
+F 0 "#PWR?" H 3850 2250 50  0001 C CNN
+F 1 "GND" V 3855 2372 50  0000 R CNN
+F 2 "" H 3850 2500 50  0001 C CNN
+F 3 "" H 3850 2500 50  0001 C CNN
+	1    3850 2500
+	0    1    1    0   
+$EndComp
+Wire Wire Line
+	4660 1900 3830 1900
+Wire Wire Line
+	4660 2100 3830 2100
+Wire Wire Line
+	4660 2300 3840 2300
+Wire Wire Line
+	4660 2500 3850 2500
+Wire Notes Line
+	5140 1490 5140 3350
+Wire Notes Line
+	5140 3350 1490 3350
+Wire Notes Line
+	1490 3350 1490 1490
+Wire Notes Line
+	1490 1490 5140 1490
+Text Label 7490 7510 0    79   ~ 0
+TOF_Sensor_Module_with_Laser&RJ45
+Text Label 8050 7310 0    79   ~ 0
+dwgno._CFS-2-7-2020-A
+Text Label 8270 7630 0    55   ~ 0
+2-7-2020
+Wire Wire Line
+	2000 3080 1770 3080
+Text Label 1870 3140 0    31   ~ 0
+blue_wire_neg
+Text Label 2410 3140 0    31   ~ 0
+red_wire_pos
+$EndSCHEMATC
