b0c3cd2995f7dd26f2c6a015f4f3cd517ee359e8
pi <illumicone@gmail.com>
2020-02-17 12:55:48 -0700

    Added YellowGiraffe pattern.  Added pattern cross-reference table so that the giraffes can use a different set of patterns.  Changed radio channel from 97 to 80 in order to avoid interference with Illumicone and Mini-Cone.

---
+++ Arduino/ThereminPixelMinion/YellowGiraffe.h
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ *                                                                             *
+ * Theremin Pixel Pattern Generator                                            *
+ *                                                                             *
+ * YellowGiraffe Class - generates a yellowish pattern for giraffes            *
+ *                                                                             *
+ * Author(s):  Ross Butler                                                     *
+ *                                                                             *
+ * February 2020                                                               *
+ *                                                                             *
+ *******************************************************************************/
+
+#pragma once
+
+#include "PixelPattern.h"
+
+
+class YellowGiraffe : public PixelPattern {
+
+  public:
+
+    static constexpr uint8_t id = 252;
+
+    // Default constructor and destructor don't do anything.
+    YellowGiraffe() {}
+    ~YellowGiraffe() {}
+          
+    // Disable copy constructor and assignment operator.
+    YellowGiraffe(const YellowGiraffe&) = delete;
+    YellowGiraffe& operator =(const YellowGiraffe&) = delete;
+
+    void update(bool widgetIsActive);
+
+  private:
+
+    static constexpr uint8_t yellowishStartHue = 30;
+    static constexpr uint8_t yellowishEndHue = 50;
+
+    static constexpr uint8_t minBrightness = 96;
+    static constexpr uint8_t maxBrightness = 255;
+
+    static constexpr uint8_t minSaturation = 128;
+    static constexpr uint8_t maxSaturation = 255;
+    
+};
